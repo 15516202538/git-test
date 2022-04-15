4ca19ca869337cb89108bb37b964797063bc7e70
ssc <1650372178@qq.com>
2022-04-15 17:36:43 +0800

    2022---ssc-01

---
+++ src/main/java/com/ssc/git/GitHubTest.java
@@ -0,0 +1,11 @@
+package com.ssc.git;
+
+/**
+ * @author ssc
+ * @create 2022-04-15 17:31
+ */
+public class GitHubTest {
+    public static void main(String[] args) {
+        System.out.println("hello GIT!");
+    }
+}
